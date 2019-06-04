6edfabf31ee7cfd84ed2017e4786a0eaa133afb0
Pablo San-Jose <lekand@gmail.com>
2019-06-04 17:39:09 +0200

    methods working

--- src/HermitianDiagonalization.jl
+++ src/HermitianDiagonalization.jl
@@ -1,46 +1,52 @@
 module HermitianDiagonalization
 
 using LinearAlgebra, SparseArrays
-using LinearMaps, IterativeSolvers, Pardiso
+using LinearMaps, IterativeSolvers, ArnoldiMethod, Arpack, KrylovKit, Pardiso
+
+export diagonalizer, reset!, IRAM_Arpack, IRAM_ArnoldiMethod, IRAM_KrylovKit, LOBPCG_IterativeSolvers
 
 ENV["OMP_NUM_THREADS"] = 4
-const useMKL = Val(true)
 
 ############################################################
-# Shift and invert solvers
+# LinearMap
 ############################################################
 
-struct ShiftInvert{S,L}
-    solver::S
-    linearmap::L
+trivialmap(h::AbstractArray) = LinearMap(h, ishermitian = true)
+
+function shiftinvert_linalg(h::AbstractArray{Tv}, shift) where {Tv}
+    fac = lu(h - Tv(shift) * I)
+    lmap = let fac = fac
+        LinearMap{Tv}((x, y) -> ldiv!(x, fac, y), size(h)...,
+                      ismutating = true, ishermitian = true)
+    end
+    return lmap, fac
 end
 
-ShiftInvert(h::AbstractArray; kw...) = ShiftInvert(h, useMKL; kw...)
-function ShiftInvert(h::AbstractArray{Tv}, usepardiso::Val{true}; verbose = false) where {Tv}
-    ps = MKLPardisoSolver()
-    finalizer(closePardiso, ps)
+function shiftinvert_pardiso(h::AbstractArray{Tv}, shift; verbose) where {Tv}
+    ps = pardisosolver(;verbose = verbose)
     _set_matrixtype!(ps, Tv)
+    hp = get_matrix(ps, h - Tv(shift) * I, :N)
+    preparesolver!(ps, hp)
+    lmap = let ps = ps, hp = hp
+        LinearMap{Tv}((x, b) -> pardiso(ps, x, hp, b), size(h)...,
+                      ismutating = true, ishermitian = true)
+    end
+    return lmap, ps
+end
+
+function pardisosolver(; verbose = false)
+    ps = MKLPardisoSolver()
+    finalizer(releasePardiso, ps)
     verbose && set_msglvl!(ps, Pardiso.MESSAGE_LEVEL_ON)
     pardisoinit(ps)
-    # set_iparm!(ps, 12, 2) # Pardiso expects CSR, Julia uses CSC
-    hp = get_matrix(ps, h, :C)
-    preparePardiso!(ps, hp)
-    invh = LinearMap{Tv}((x, b) -> pardiso(ps, x, hp, b), size(h)...,
-                      ismutating = true, ishermitian = true)
-    return ShiftInvert(ps, invh)
-end
-
-function ShiftInvert(h::AbstractArray{Tv}, usepardiso::Val{false}) where {Tv}
-    fac = lu(h)
-    l = LinearMap{Tv}((x, y) -> ldiv!(x, fac, y), size(h)...,
-                      ismutating = true, ishermitian = true)
-    return ShiftInvert(fac, l)
+    set_iparm!(ps, 12, 2) # Pardiso expects CSR, Julia uses CSC
+    return ps
 end
 
 _set_matrixtype!(ps, ::Type{<:AbstractFloat}) = set_matrixtype!(ps, Pardiso.REAL_SYM_INDEF)
 _set_matrixtype!(ps, ::Type{<:Complex}) = set_matrixtype!(ps, Pardiso.COMPLEX_HERM_INDEF)
 
-function preparePardiso!(ps, hp::AbstractArray{Tv}) where {Tv}
+function preparesolver!(ps, hp::AbstractArray{Tv}) where {Tv}
     b = Vector{Tv}(undef, size(hp, 1))
     set_phase!(ps, Pardiso.ANALYSIS_NUM_FACT)
     pardiso(ps, hp, b)
@@ -48,24 +54,136 @@ function preparePardiso!(ps, hp::AbstractArray{Tv}) where {Tv}
     return ps
 end
 
-closePardiso(ps) = (set_phase!(ps, Pardiso.RELEASE_ALL); pardiso(ps))
+function releasePardiso(ps)
+    original_phase = get_phase(ps)
+    set_phase!(ps, Pardiso.RELEASE_ALL);
+    pardiso(ps)
+    set_phase!(ps, original_phase)
+    return ps
+end
 
 ############################################################
-# Shift and invert solvers
+# Diagonalizers
 ############################################################
 
-# function diagonalize(h::AbstractArray; 
-#                      check = false, nev = 6, sigma = 0.0, codiag = missing, verbose = false)
-#     check && (ishermitian(h) && error("Matrix is non-Hermitian"))
-#     si = linearmap(h, sigma, useMKL)
-# end
+abstract type AbstractEigenMethod{Tv} end
 
-# linearmap(h::AbstractArray, ::Missing, _) = LinearMap(h, ishermitian = true)
-# function linearmap(h::AbstactArray, sigma::Number, ::useMKL)
-#     LinearMap()
-# end
+mutable struct Diagonalizer{M<:AbstractEigenMethod,Tv,L<:LinearMap{Tv},C,E}
+    matrix::SparseMatrixCSC{Tv, Int}
+    method::M
+    lmap::L
+    point::Float64
+    codiag::C
+    engine::E   # Optional support for lmap (e.g. Pardiso solver or factorization)
+end
 
+Base.show(io::IO, d::Diagonalizer{M,Tv}) where {M,Tv} = print(io, 
+"Diagonaliser{$M} for $(size(d.matrix)) Hermitian matrix around point $(d.point)")
 
+function diagonalizer(h::AbstractArray{Tv}, ::Type{S} = defaultmethod; 
+                      point = 0.0, codiag = missing, pardiso = true, 
+                      verbose = false) where {Tv,S<:AbstractEigenMethod}
+    ishermitian(h) || error("Matrix is non-Hermitian")
+    if isfinite(point)
+        lmap, engine = pardiso ? shiftinvert_pardiso(h, point; verbose = verbose) : 
+                                 shiftinvert_linalg(h, point)
+    else
+        lmap, engine = trivialmap(h), missing
+    end
+    return Diagonalizer(h, S(lmap), lmap, point, codiag, engine)
+end
 
+# # Resets preconditioner, optionally changes method - fails to call constructor (param)
+# reset!(d::Diagonalizer{M}, ::Type{M2} = M) where {M<:AbstractEigenMethod, M2<:AbstractEigenMethod} = 
+#     (@show M; d.method = M2(d.lmap)) 
+
+############################################################
+# Methods
+############################################################
+
+struct IRAM_ArnoldiMethod{Tv} <: AbstractEigenMethod{Tv}
+end
+IRAM_ArnoldiMethod(lmap::LinearMap{Tv}) where {Tv} = IRAM_ArnoldiMethod{Tv}()
+
+struct IRAM_Arpack{Tv} <: AbstractEigenMethod{Tv}
+    precond::Vector{Tv}
+end
+IRAM_Arpack(lmap::LinearMap{Tv}) where {Tv} = IRAM_Arpack(rand(Tv, size(lmap, 2)))
+
+struct IRAM_KrylovKit{Tv} <: AbstractEigenMethod{Tv}
+    precond::Vector{Tv}
+end
+IRAM_KrylovKit(lmap::LinearMap{Tv}) where {Tv} = IRAM_KrylovKit(rand(Tv, size(lmap, 2)))
+
+struct LOBPCG_IterativeSolvers{Tv} <: AbstractEigenMethod{Tv}
+    precond::Matrix{Tv}
+end
+
+LOBPCG_IterativeSolvers(lmap::LinearMap{Tv}, nev = 1) where {Tv} = 
+    LOBPCG_IterativeSolvers(rand(Tv, size(lmap,1), nev))
+
+const defaultmethod = IRAM_KrylovKit
+
+############################################################
+# Method interfaces
+############################################################
+
+function (d::Diagonalizer{<:IRAM_Arpack,Tv})(nev::Integer; kw...) where {Tv}
+    if isfinite(d.point)
+        which = :LM
+        # sigma = real(Tv) === Tv ? d.point : d.point + 1.0im
+        sigma = d.point
+    elseif point > 0
+        which = :LR
+        sigma = nothing
+    else
+        which = :SR
+        sigma = nothing
+    end
+    λs, ϕs, _ = eigs(d.matrix; nev = nev, sigma = sigma, which = which, 
+                             v0 = d.method.precond, kw...)
+    d.method.precond .= zero(Tv)
+    foreach(ϕ -> (d.method.precond .+= ϕ), eachcol(ϕs))
+    return Eigen(real(λs), ϕs)
+end
+
+function (d::Diagonalizer{<:IRAM_ArnoldiMethod,Tv})(nev::Integer; kw...) where {Tv}
+    if isfinite(d.point)
+        which = LM()
+    else
+        which = d.point > 0 ? LR() : SR()
+    end
+    decomp, _ = partialschur(d.lmap; nev = nev, which = which, kw...)
+    λs = real.(decomp.eigenvalues)
+    ϕs = decomp.Q
+    isfinite(d.point) && (λs .= 1 ./ λs .+ d.point)
+    return Eigen(λs, ϕs)
+end
+
+function (d::Diagonalizer{<:IRAM_KrylovKit, Tv})(nev::Integer; kw...) where {Tv}
+    if isfinite(d.point)
+        λs, ϕv, _ = eigsolve(x -> d.lmap * x, d.method.precond, nev; kw...)
+                            #  ishermitian = true, kw...) # ishermitian fails
+        λs .= 1 ./ λs .+ d.point
+    else
+        λs, ϕv, _ = eigsolve(d.matrix, d.method.precond, nev, d.point > 0 ? :LR : :SR, 
+                             Lanczos(kw...))
+    end
+    d.method.precond .= zero(Tv)
+    foreach(ϕ -> (d.method.precond .+= ϕ), ϕv)
+    return Eigen(real(λs), hcat(ϕv))
+end
+
+function (d::Diagonalizer{<:LOBPCG_IterativeSolvers, Tv})(nev::Integer; largest = true, kw...) where {Tv}
+    if size(d.method.precond) != (size(d.matrix, 1), nev)
+        d.method = LOBPCG_IterativeSolvers(d.lmap, nev) # reset preconditioner
+    end
+    largest = ifelse(isfinite(d.point), largest, d.point > 0)
+    result = lobpcg(d.lmap, I, largest, d.method.precond; kw...)
+    λs, ϕs = result.λ, result.X
+    isfinite(d.point) && (λs .= 1 ./ λs .+ d.point)
+    foreach(i -> (d.method.precond[i] = ϕs[i]), eachindex(d.method.precond))
+    return Eigen(real(λs), ϕs)
+end
 
 end # module
